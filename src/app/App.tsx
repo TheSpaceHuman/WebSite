c6d031d0261fd18f134de720fb7ab9a542539224
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2024-01-08 23:50:54 +0300

    changed designation theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/index.scss';
-import cn from 'classnames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Header } from 'widgets';
 import { AppRouter } from 'app/providers/router';
@@ -8,7 +7,7 @@ import { AppRouter } from 'app/providers/router';
 const App = () => {
 	const {theme} = useTheme();
 	return (
-		<div className={cn('app', `theme--${theme}`)}>
+		<div className="app" data-theme={theme}>
 			<Header />
 			<AppRouter />
 		</div>
